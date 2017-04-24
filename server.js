f3d82bb7cb093a57507d15a2fd86ad6099ca27ac
Manuel Kraus <manuel.c.kraus@gmail.com>
2017-04-24 13:15:52 +0200

    add start script to package.json and set up heroku as port in server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.set("view engine","hbs");
@@ -50,4 +52,7 @@ app.get("/bad", function(req,res) {
 	})
 })
 
-app.listen(3000);
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
+});
+
